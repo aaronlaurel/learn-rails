ec7e635829afc6d25b9f3d5f7927d370d238c70c
ZongyiLuo <zongyil@andrew.cmu.edu>
2013-09-12 13:15:20 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bootstrap-sass'
+gem 'figaro'
+gem 'high_voltage'
+gem 'simple_form', '>= 3.0.0.rc'
+gem 'thin'
+
+gem 'activerecord-tableless'
+gem 'google_drive'
+gem 'gibbon'
+group :development do
+  gem 'sqlite3'
+  gem 'better_errors'
+  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
+  gem 'quiet_assets'
+end
+group :production do
+	gem 'pg'
+	gem 'rails_on_heroku' 
+	gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsBootstrap::Application.config.secret_key_base = 'bb60d2a87563f98f140ec7ffab139cc9ac349184207117ba48c265e30fb61d3be94bed72bd393ccd1a54cbcd9fcf756864b34af4aa2e761310b29445d5abf4b4'
